368df1c746af3ce7e5a1a95635aaff3ce2bdc4e7
dannycahyo <dannydwicahyono@gmail.com>
2023-04-29 12:37:47 +0700

    feat: finishing the tech speaker sections

--- src/components/about/TechSpeaker.tsx
+++ src/components/about/TechSpeaker.tsx
@@ -1,7 +1,164 @@
-import React from "react";
+import Image from "next/image";
+import {
+  Heading,
+  Button,
+  Text,
+  Flex,
+  Box,
+  Grid,
+  useColorModeValue,
+  Divider,
+  Container,
+  Center,
+  Tag,
+  HStack,
+  TagLabel,
+  IconButton,
+} from "@chakra-ui/react";
+
+import { FaYoutube } from "react-icons/fa";
+import ChakraCarousel from "src/containers/ChakraCarousel";
+import { techSpeakerData } from "src/constant/techSpeaker";
 
 const TechSpeaker = () => {
-  return <div>TechSpeaker</div>;
+  const bgBasedOnColorMode = useColorModeValue("#F7F7F7F7", "personalDark.400");
+
+  const borderColorBasedOnColorMode = useColorModeValue(
+    "personalLight.300",
+    "personalDark.100"
+  );
+
+  return (
+    <Container
+      centerContent
+      maxW={{ base: "fit-content", md: "2xl", lg: "3xl", xl: "4xl" }}
+      p="0px"
+      pt="4"
+    >
+      <Grid justifyContent="center" alignItems="center">
+        <Heading size={{ base: "lg", md: "xl" }}>My Inspiring Talks</Heading>
+        <Divider
+          borderBottomWidth="2px"
+          mt="4"
+          borderColor={borderColorBasedOnColorMode}
+        />
+      </Grid>
+      <ChakraCarousel gap={32}>
+        {techSpeakerData.map((event) => (
+          <Box
+            mt={{ base: "20px", md: "40px" }}
+            width={{ base: "full", md: "80%" }}
+            marginX="auto"
+            key={event.title}
+            bg={bgBasedOnColorMode}
+            borderRadius="40px"
+            p={{ base: "20px", md: "40px" }}
+            pt="10px"
+          >
+            <Flex
+              justifyContent="space-between"
+              flexDir={{ base: "column", md: "row" }}
+              alignItems="center"
+              mt="4"
+            >
+              <Box>
+                <Heading
+                  fontSize={{ base: "xl", md: "md", lg: "lg", xl: "xl" }}
+                  lineHeight={{ base: "inherit" }}
+                  textAlign="left"
+                >
+                  {event.title}
+                </Heading>
+                <HStack mt="1">
+                  <Box
+                    as="div"
+                    w="12px"
+                    height="12px"
+                    bg="#68D94A"
+                    borderRadius="full"
+                  />
+                  <Text size="sm" textAlign="left">
+                    {event.date}
+                  </Text>
+                </HStack>
+              </Box>
+              <HStack
+                gap={1}
+                mt={{ base: "3", md: "0" }}
+                alignSelf="flex-start"
+              >
+                {event.topics.map((topic) => (
+                  <Tag
+                    size="lg"
+                    colorScheme="cyan"
+                    key={topic}
+                    borderRadius="full"
+                  >
+                    <TagLabel>
+                      <Text size="xs">{topic}</Text>
+                    </TagLabel>
+                  </Tag>
+                ))}
+              </HStack>
+            </Flex>
+
+            <Center pt="4">
+              <Image
+                alt={event.title}
+                width={400}
+                height={400}
+                src={event.image}
+                style={{ borderRadius: "20px", width: "100%" }}
+              />
+            </Center>
+
+            <Flex justifyContent="space-between" alignItems="center" mt="4">
+              <HStack gap={2}>
+                <Button
+                  borderRadius="25px"
+                  bg="linear-gradient(90.55deg, #256BF4 -13.39%, #3AC1DE 99.74%)"
+                  _hover={{
+                    bg: "linear-gradient(90.55deg, #256BF4 -13.39%, #3AC1DE 99.74%)",
+                    opacity: 0.8,
+                  }}
+                  color="white"
+                  as="a"
+                  href={event.eventLink}
+                  target="_blank"
+                  size={{ base: "md", lg: "lg" }}
+                >
+                  <Text size="sm" fontWeight="600">
+                    See Detail
+                  </Text>
+                </Button>
+                {event.videoLink !== undefined ? (
+                  <IconButton
+                    as="a"
+                    href={event.videoLink}
+                    aria-label="event video"
+                    icon={<FaYoutube fontSize="1.25rem" />}
+                    variant="outline"
+                    colorScheme="red"
+                  />
+                ) : null}
+              </HStack>
+              <Box as="div">
+                <Heading
+                  size={{ base: "md", lg: "xl" }}
+                  as="a"
+                  href={event.organizerLink}
+                  target="_blank"
+                >
+                  {event.organizer}
+                </Heading>
+                <Divider borderBottomWidth="4px" mt="4" borderColor="yellow" />
+              </Box>
+            </Flex>
+          </Box>
+        ))}
+      </ChakraCarousel>
+    </Container>
+  );
 };
 
 export default TechSpeaker;
---
+++ src/constant/techSpeaker.ts
@@ -0,0 +1,36 @@
+type TechSpeakerData = {
+  title: string;
+  date: string;
+  organizer: string;
+  image: string;
+  topics: string[];
+  organizerLink?: string;
+  eventLink: string;
+  videoLink?: string;
+};
+
+const techSpeakerData: TechSpeakerData[] = [
+  {
+    title: "Getting Started to Functional Programming",
+    date: "14 January 2023",
+    eventLink: "https://www.instagram.com/p/CnD921Ey8t8/",
+    topics: ["JavaScript", "Functional"],
+    image:
+      "/assets/image/tech-speaker/Getting Started to Functional Programming.jpeg",
+    organizer: "Gatherloop",
+    organizerLink: "https://gatherloop.co/",
+  },
+  {
+    title: "Getting Started with JavaScript",
+    date: "11 March 2023",
+    topics: ["JavaScript", "Basic"],
+    eventLink: "https://www.instagram.com/p/CpZ5EzMv7ib/",
+    image: "/assets/image/tech-speaker/Getting Started To JavaScript.jpeg",
+    organizer: "Gatherloop",
+    organizerLink: "https://gatherloop.co/",
+    videoLink: "https://www.youtube.com/watch?v=qH39M3Jn6hk&t=563s",
+  },
+];
+
+export { techSpeakerData };
+export type { TechSpeakerData };
