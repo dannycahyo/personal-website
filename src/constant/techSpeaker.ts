9bcedce3edc0958c9c7de26a43da136c0c9b00a2
dannycahyo <dannydwicahyono@gmail.com>
2023-04-29 13:00:33 +0700

    fix: change the order of the tech speaker data

--- src/constant/techSpeaker.ts
+++ src/constant/techSpeaker.ts
@@ -11,16 +11,6 @@ type TechSpeakerData = {
 
 const techSpeakerData: TechSpeakerData[] = [
   {
-    title: "Getting Started to Functional Programming",
-    date: "14 January 2023",
-    eventLink: "https://www.instagram.com/p/CnD921Ey8t8/",
-    topics: ["JavaScript", "Functional"],
-    image:
-      "/assets/image/tech-speaker/Getting Started to Functional Programming.jpeg",
-    organizer: "Gatherloop",
-    organizerLink: "https://gatherloop.co/",
-  },
-  {
     title: "Getting Started with JavaScript",
     date: "11 March 2023",
     topics: ["JavaScript", "Basic"],
@@ -29,6 +19,16 @@ const techSpeakerData: TechSpeakerData[] = [
     organizer: "Gatherloop",
     organizerLink: "https://gatherloop.co/",
     videoLink: "https://www.youtube.com/watch?v=qH39M3Jn6hk&t=563s",
+  },
+  {
+    title: "Getting Started to Functional Programming",
+    date: "14 January 2023",
+    eventLink: "https://www.instagram.com/p/CnD921Ey8t8/",
+    topics: ["JavaScript", "Functional"],
+    image:
+      "/assets/image/tech-speaker/Getting Started to Functional Programming.jpeg",
+    organizer: "Gatherloop",
+    organizerLink: "https://gatherloop.co/",
   },
 ];
 
