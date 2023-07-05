c5c433024393275f87a505c5b7dadd315c8a5bb1
dannycahyo <dannydwicahyono@gmail.com>
2023-07-05 13:24:19 +0700

    feat: sorting the blog to desc

--- src/constant/blog.ts
+++ src/constant/blog.ts
@@ -8,12 +8,13 @@ type BlogPost = {
 
 const blogPost: BlogPost[] = [
   {
-    title: "Why Bother Learning Data Structures And Algorithms?",
-    slug: "why-bother-learning-data-structures-and-algorithms",
+    title:
+      "How I Became a Professional Software Engineer from a Physical Education Background",
+    slug: "how-i-became-a-professional-software-engineer-from-a-physical-education-background",
     imageURL:
-      "https://images.unsplash.com/photo-1515187029135-18ee286d815b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-    date: "2023-06-04",
-    readEstimate: 5,
+      "https://res.cloudinary.com/du5jbmwz5/image/upload/v1686311572/Professional%20Software%20Engineer.jpg",
+    date: "2023-06-10",
+    readEstimate: 8,
   },
   {
     title:
@@ -25,13 +26,12 @@ const blogPost: BlogPost[] = [
     readEstimate: 7,
   },
   {
-    title:
-      "How I Became a Professional Software Engineer from a Physical Education Background",
-    slug: "how-i-became-a-professional-software-engineer-from-a-physical-education-background",
+    title: "Why Bother Learning Data Structures And Algorithms?",
+    slug: "why-bother-learning-data-structures-and-algorithms",
     imageURL:
-      "https://res.cloudinary.com/du5jbmwz5/image/upload/v1686311572/Professional%20Software%20Engineer.jpg",
-    date: "2023-06-10",
-    readEstimate: 8,
+      "https://images.unsplash.com/photo-1515187029135-18ee286d815b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    date: "2023-06-04",
+    readEstimate: 5,
   },
 ];
 
