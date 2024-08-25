68df9a6b4a694b20ff5381f7ca4daef62eaacd08
dannycahyo <dannydwicahyono@gmail.com>
2024-08-25 15:08:51 +0700

    feat: unable TechSpeaker component on About page

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -15,8 +15,14 @@ const About = () => {
           name="description"
           content="Welcome to my About page! Here, you'll find everything you need to know about me and my mission. First and foremost, I'm committed to achieving my goals and making a positive impact on the world. I believe that by continuously learning and sharing knowledge with others, we can all work together to create a better future. As a content creator, I'm passionate about using my skills and expertise to create valuable resources for others. Whether it's through blog posts, podcasts, or videos, I'm dedicated to sharing my knowledge and experiences with the world. In addition to my content creation work, I also speak at conferences and lead workshops to help others achieve their goals. It's important to me to give back to the community and help others succeed in their own endeavors. Finally, at the core of everything I do are my values. Integrity, collaboration, and kindness are just a few of the values that guide me in both my personal and professional life. Thank you for visiting my About page, and I look forward to connecting with you further!"
         />
-        <link rel="canonical" href="https://dannydwicahyono.com/about" />
-        <meta property="og:url" content="https://dannydwicahyono.com/about" />
+        <link
+          rel="canonical"
+          href="https://dannydwicahyono.com/about"
+        />
+        <meta
+          property="og:url"
+          content="https://dannydwicahyono.com/about"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="About Danny Dwi Cahyono" />
         <meta
@@ -30,7 +36,7 @@ const About = () => {
       </Head>
       <Heroes />
       <ContentCreator />
-      <TechSpeaker />
+      {/* <TechSpeaker /> */}
       <CoreValues />
       {/* TODO: Enable this component once the UI is finished */}
       {/* <Hobbies /> */}
