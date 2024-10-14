048de0e648e6c2eabd51c76bdba9b7d89277bd80
dannycahyo <dannydwicahyono@gmail.com>
2024-10-14 19:42:14 +0700

    Revert "feat: Add Vercel-CDN-Cache-Control header to /blog page server-side props"
    This reverts commit e29d6d16f870968fa322bfae561f253a8f1ffb14.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/blog",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
