5c50e845ccc7cb9b27e675db8066df259035d9c0
dannycahyo <dannydwicahyono@gmail.com>
2024-10-14 19:15:44 +0700

    feat: Add Vercel-CDN-Cache-Control header to About page server-side props

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,17 @@
 import About from "src/components/about";
+import type { GetServerSidePropsContext } from "next";
 
 function AboutPage() {
   return <About />;
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps({
+  res,
+}: GetServerSidePropsContext) {
+  res.setHeader(
+    "Vercel-CDN-Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59",
+  );
   return { props: {} };
 }
 
